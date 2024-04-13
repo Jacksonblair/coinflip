9715ffbd889ead9c795ebb18d46cc8a34d74fe02
Jacksonblair <jackson.blair@live.com>
2024-04-13 13:04:08 +1000

    pls god

--- src/components/lane/lane.tsx
+++ src/components/lane/lane.tsx
@@ -14,6 +14,7 @@ export const Lane = (props: ComponentProps<"div">) => {
                 justifyContent: "flex-end",
                 display: "flex",
                 margin: "0 auto",
+                minHeight: "500px",
             }}
             {...props}
         />
